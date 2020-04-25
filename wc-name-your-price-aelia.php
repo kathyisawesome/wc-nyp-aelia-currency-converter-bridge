3b34d4afa029ed499129f166b5cece111bf34120
Kathy Daring <helgatheviking@gmail.com>
2020-04-25 16:13:07 -0600

    Add and save different prices per currency

--- wc-name-your-price-aelia.php
+++ wc-name-your-price-aelia.php
@@ -162,9 +162,143 @@ class WC_NYP_Aelia_CC {
 		return apply_filters( 'wc_aelia_cs_convert', $price, $from_currency, $to_currency );
 	}
 
+
+    /*-----------------------------------------------------------------------------------*/
+	/* Write Panel / metabox */
+	/*-----------------------------------------------------------------------------------*/
+
+
+	/**
+	 * Add maximum inputs to product metabox
+	 *
+	 * @param  object WC_Product $product_object
+	 * @param  bool $show_billing_period_options
+	 * @param  mixed int|false $loop - for use in variations
+	 * @return print HTML
+	 * @since  0.4.0
+	 */
+	public static function pricing_options( $product_object, $show_billing_period_options = false, $loop = false ) {
+
+		$currencies = apply_filters('wc_aelia_cs_enabled_currencies', array() );
+
+		$default_currency = get_option( 'woocommerce_currency' );
+
+		if( count( $currencies ) > 1 ) { ?>
+
+			<div class="wc_aelia_cs_product_prices clearfix hide_if_subscription">
+
+				<div class="product_currency_prices_header">
+					<h3><?php esc_html_e( 'Price in specific currencies', 'wc_name_your_price'); ?></h3>
+
+					<?php
+
+					foreach( $currencies as $currency ) {
+
+						if( $currency === $default_currency ) {
+							continue;
+						}
+
+						// Suggested Price.
+						woocommerce_wp_text_input( array(
+							'id' => is_int( $loop ) ? "variation_suggested_price_currency[$currency][$loop]" : "_suggested_price_currency[$currency]",
+							'class' => 'wc_input_price short',
+							'wrapper_class' => is_int( $loop ) ? 'form-row form-row-first' : '',
+							'placeholder' => __( 'Auto', 'wc_name_your_price' ),
+							'label' => __( 'Suggested Price', 'wc_name_your_price') . ' ('. $currency .')' ,
+							'desc_tip' => 'true',
+							'description' => __( 'Price to pre-fill for customers.  Leave blank to not suggest a price.', 'wc_name_your_price' ),
+							'data_type' => 'price',
+							'value' => $product_object->get_meta( '_suggested_price_' . $currency, true )
+						) );
+
+						// Minimum Price.
+						woocommerce_wp_text_input( array(
+							'id' => is_int( $loop ) ? "variation_min_price_currency[$loop][$currency]" : "_min_price_currency[$currency]",
+							'class' => 'wc_input_price short',
+							'wrapper_class' => is_int( $loop ) ? 'form-row form-row-last' : '',
+							'placeholder' => __( 'Auto', 'wc_name_your_price' ),
+							'label' => __( 'Minimum Price', 'wc_name_your_price') . ' (' . $currency . ')',
+							'desc_tip' => 'true',
+							'description' =>  __( 'Lowest acceptable price for product. Leave blank to not enforce a minimum. Must be less than or equal to the set suggested price.', 'wc_name_your_price' ),
+							'data_type' => 'price',
+							'value' => $product_object->get_meta( '_min_price_' . $currency, true )
+						) );
+
+						// Maximum Price.
+						woocommerce_wp_text_input( array(
+							'id' => is_int( $loop ) ? "variation_maximum_price_currency[$loop][$currency]" : "_maximum_price_currency[$currency]",
+							'class' => 'wc_input_price short',
+							'wrapper_class' => is_int( $loop ) ? 'form-row form-row-first' : '',
+							'placeholder' => __( 'Auto', 'wc_name_your_price' ),
+							'label' => __( 'Maximum Price', 'wc_name_your_price') . ' (' . $currency . ')',
+							'desc_tip' => 'true',
+							'description' =>  __( 'Highest acceptable price for product. Leave blank to not enforce a maximum.', 'wc_name_your_price' ),
+							'data_type' => 'price',
+							'value' => $product_object->get_meta( '_maximum_price_' . $currency, true )
+						) );
+
+						echo '<hr>';
+
+					}
+
+					?>
+				</div>
+		</div>
+
+	<?php }
+	}
+
+	/**
+	 * Save extra meta info
+	 *
+	 * @param object $product
+	 * @return void
+	 * @since 1.0 (renamed in 2.0)
+	 */
+	public static function save_product_meta( $product ) {
+
+		// Text Field - Amounts in each currency
+		$suggested_prices = $_POST['_suggested_price_currency'];
+
+		if( ! empty( $suggested_prices ) ) {
+
+			// Save the fee amount for each currency
+			foreach( $suggested_prices as $currency => $amount ) {
+				$product->update_meta_data( '_suggested_price_' . $currency, wc_format_decimal( $amount ) );
+			}
+		
+		}
+
+		// Text Field - Amounts in each currency
+		$minimum_prices = $_POST['_min_price_currency'];
+
+		if( ! empty( $minimum_prices ) ) {
+
+			// Save the fee amount for each currency
+			foreach( $minimum_prices as $currency => $amount ) {
+				$product->update_meta_data( '_min_price_' . $currency, wc_format_decimal( $amount ) );
+			}
+		
+		}
+
+		// Text Field - Amounts in each currency
+		$maximum_prices = $_POST['_maximum_price_currency'];
+
+		if( ! empty( $maximum_prices ) ) {
+
+			// Save the fee amount for each currency
+			foreach($maximum_prices as $currency => $amount) {
+				$product->update_meta_data( '_maximum_price_' . $currency, wc_format_decimal( $amount ) );
+			}
+		
+		}
+
+	}
+
+
 } //end class: do not remove or there will be no more guacamole for you
 
 endif; // end class_exists check
 
 // Launch the whole plugin.
-add_action( 'wc_name_your_price_loaded', array( 'WC_NYP_Aelia_CC', 'init' ) );
+add_action( 'plugins_loaded', array( 'WC_NYP_Aelia_CC', 'init' ) );
